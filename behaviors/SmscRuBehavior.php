47f7c447ea1a4139dd5ad776248e73f3c992d3a4
Ivan Chuchman <ivan.chuchman@gmail.com>
2017-07-04 16:00:26 +0300

    add call support

--- behaviors/SmscRuBehavior.php
+++ behaviors/SmscRuBehavior.php
@@ -13,17 +13,42 @@ class SmscRuBehavior extends Behavior
         parent::init();
     }
 
-    public static function sendSMS($data)
+    private static function confirmCode($phone)
     {
         $config = Yii::$app->params['smsc'];
 
+        $params = [
+            'charset'   => $config['charset'],
+            'fmt'       => $config['fmt'],
+            'login'     => $config['login'],
+            'psw'       => $config['password'],
+            'phones'    => urlencode($phone)
+        ];
+
+        if ($config['call'])
+        {
+            $params['call'] = 1;
+            $params['mes']  = 'code';
+        }
+
+        else {
+            $code           = mt_rand(1,9).mt_rand(0,9).mt_rand(0,9).mt_rand(0,9).mt_rand(0,9);
+            $params['mes']  = urlencode(\Yii::t('smsc.ru', 'confirm_code_txt').': '.$code);
+        }
+
+        $fields = http_build_query($params, '', '&');
+
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        //curl_setopt($ch, CURLOPT_URL, "http://smsc.ru/sys/send.php?flash=1&charset=utf-8&login=".$config['login']."&psw=".$config['password']."&phones=".urlencode($data['phone'])."&mes=".urlencode($data['message']));
-        curl_setopt($ch, CURLOPT_URL, "http://smsc.ru/sys/send.php?charset=utf-8&login=".$config['login']."&psw=".$config['password']."&phones=".urlencode($data['phone'])."&mes=".urlencode($data['message']));
+        curl_setopt($ch, CURLOPT_URL, $config['url']."?".$fields);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $result = curl_exec($ch);
         curl_close($ch);
+
+        $result         = json_decode($result, true);
+        $result['code'] = ($config['call']) ? $result['code'] : $code;
+
+        return $result;
     }
 }
